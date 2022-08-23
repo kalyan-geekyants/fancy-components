6bdd90bb212da68fc948937cf694fba704fbe39b
kalyankanike <kalyankanike@geekyants.com>
2022-08-23 14:47:58 +0530

    added input component

--- src/index.jsx
+++ src/index.jsx
@@ -79,4 +79,26 @@ FancyButton.propTypes = {
     onClick: PropTypes.func.isRequired
 }
 
-export {FancyButton};
+const Input = styled.input.attrs((props) => ({
+    // we can define static props
+    type: "text",
+  
+    // or we can define dynamic ones
+    size: props.size || "0.5em",
+  }))`
+    color: palevioletred;
+    font-size: 1em;
+    border: 2px solid #ccc;
+    border-radius: 3px;
+  
+    /* here we use the dynamically computed prop */
+    margin: ${(props) => props.size};
+    padding: ${(props) => props.size};
+  `;
+  
+  const FancyInput = ({placeholder,onChange, ...props}) => {
+    return <Input  placeholder={placeholder} {...props} onChange={onChange} />;
+  };
+  
+
+export {FancyButton, FancyInput};
